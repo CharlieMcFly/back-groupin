ad5046383021aff650f1391d26acbf475ce99d98
Charlie Quetstroey <cquetstroey@hotmail.fr>
2016-11-15 17:32:47 +0100

    feat(saveuser) : changement du json avec les datas données par l'authentification

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var firebase = require('../firebase/firebase.js');
-var database = firebase.database();
+var userDB = firebase.database().ref().child('users');
 
 /**
  * Création de l'utilisateur : POST => /users
+ *
+ * Problème rencontré : Lors de la création de l'utilisateur dans la database de firebase, il était impossible de mettre
+ * une clé avec une adresse email, donc on a modifié cette adresse pour qu'elle puisse devenir un uid, en remplacant @ et .
+ * par des -.
  */
 router.post('/', function(req, res){
 
@@ -13,23 +17,45 @@ router.post('/', function(req, res){
     email : req.body.email,
     photoUrl : req.body.photoURL,
     provider : req.body.providerId,
-    uid : req.body.uid,
+    // Afin de pouvoir créer un uid avec l'utilisateur sans facebook et google
+    uid : req.body.uid.replace("@", "-").replace(".", "-")
   };
 
-  var updates = {};
-  firebase.database().ref().child('users').orderByChild('uid').equalTo(user.uid).on('value', function(snapshot) {
-    if (snapshot.val() == null) {
-      // Get a key for a new Post.
-      firebase.database().ref().child('users').push(user);
+  var isRegister = false;
+  userDB.once('value', function(snapshot) {
+    if (snapshot.val()) {
+        // Itération de tous les users
+        snapshot.forEach(function(snap){
+            if(snap.val().uid === user.uid){
+              isRegister = true;
+              res.sendStatus(200);
+            }
+        });
+        if(!isRegister) {
+            firebase.database().ref('users/' + user.uid).set(user);
+            res.sendStatus(200);
+        }
+    }else{
+      firebase.database().ref('users/' + user.uid).set(user);
+      res.sendStatus(200);
     }
   });
-
-  res.sendStatus(200);
-
 });
 
-router.get('/', function(req, res){
-  res.sendStatus(200);
+/**
+ * Récupéreration de l'utilisateur : GET => /users/:uid
+ */
+router.get('/:uid', function(req, res){
+
+    var uid = req.params.uid.replace("@", "-").replace(".", "-");
+    var user = userDB.child(uid).once('value').then(function(snapshot){
+      if(snapshot.val())
+        res.send(snapshot.val());
+      else{
+        res.send({});
+      }
+    });
+
 });
 
 module.exports = router;
