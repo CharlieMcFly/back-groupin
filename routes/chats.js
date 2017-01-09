b936db2c40dd87b5247e8d9bd620487f07f01b85
Charlie Quetstroey <cquetstroey@hotmail.fr>
2017-01-09 01:40:23 +0100

    refactor(votes & chats)  : maj de des apis pour les votes et le chat du groupe
    modification de la charte graphique

--- routes/chats.js
+++ routes/chats.js
@@ -5,58 +5,110 @@ var express = require('express');
 var router = express.Router();
 var firebase = require('../firebase/firebase.js');
 var database = firebase.database();
-var eventsDB = database.ref().child('events');
-var groupsDB = database.ref().child('groups');
-var usersDB = database.ref().child('users');
-var chatsDB = database.ref().child('chats');
+var eventDB = database.ref().child('events');
+var groupDB = database.ref().child('groups');
+var userDB = database.ref().child('users');
+var chatDB = database.ref().child('chats');
+
+/**
+ * Récupérer le chat d'un groupe
+ */
+router.get('/users/:uid/groups/:key', function(req, res){
+
+    var uid = req.params.uid;
+    var groupid = req.params.key;
+
+    groupDB.child(groupid).once("value", function(group){
+        userDB.once("value", function(users){
+            chatDB.once('value', function(msgs){
+                var all_messages = msgs.val();
+                var my_group = group.val();
+                var all_users = users.val();
+                var my_user = all_users[uid];
+                var tabMsg = [];
+                if(my_group){
+                    if(all_messages){
+                        if(my_group.messages){
+                            for(var m in my_group.messages){
+                                if(all_messages[m]){
+                                    if(all_users){
+                                        if(all_users[all_messages[m].auteur]){
+                                            all_messages[m].auteur = all_users[all_messages[m].auteur];
+                                        }
+                                    }
+                                    all_messages[m].date = new Date(all_messages[m].date).toString();
+                                    tabMsg.push(all_messages[m]);
+                                }
+                            }
+                        }
+                    }
+                }
+                var result = {
+                    "user" : my_user,
+                    "messages" : tabMsg
+                };
+                res.send(result);
+            });
+        });
+    });
 
 
+});
+
+/**
+ * Poster un message
+ */
 router.post('/', function(req, res){
 
     var key = req.body.id;
     if (key == undefined){
-        key = chatsDB.push().key;
-        chatsDB.child(key).child("id").set(key);
+        key = chatDB.push().key;
+        chatDB.child(key).child("id").set(key);
     }
 
     var uid = req.body.uid;
     var message = req.body.message;
     var groupId = req.body.groupId;
 
-    groupsDB.child(groupId).child("messages").child(key).set(true);
+    groupDB.child(groupId).child("messages").child(key).set(true);
 
-    chatsDB.child(key).child("auteur").set(uid);
-    chatsDB.child(key).child("message").set(message);
-    chatsDB.child(key).child("date").set(firebase.database.ServerValue.TIMESTAMP);
+    chatDB.child(key).child("auteur").set(uid);
+    chatDB.child(key).child("message").set(message);
+    chatDB.child(key).child("date").set(firebase.database.ServerValue.TIMESTAMP);
 
-    groupsDB.once('value', function(groups){
-
-        chatsDB.once('value', function(msgs){
-
-            var messages = {
-                "messages" : msgs.val(),
-                "groups" : groups.val()
-            };
-            res.send(messages);
-
+    groupDB.child(groupId).once("value", function(group){
+        userDB.once("value", function(users){
+            chatDB.once('value', function(msgs){
+                var all_messages = msgs.val();
+                var my_group = group.val();
+                var all_users = users.val();
+                var my_user = all_users[uid];
+                var tabMsg = [];
+                if(my_group){
+                    if(all_messages){
+                        if(my_group.messages){
+                            for(var m in my_group.messages){
+                                if(all_messages[m]){
+                                    if(all_users){
+                                        if(all_users[all_messages[m].auteur]){
+                                            all_messages[m].auteur = all_users[all_messages[m].auteur];
+                                        }
+                                    }
+                                    all_messages[m].date = new Date(all_messages[m].date).toString();
+                                    tabMsg.push(all_messages[m]);
+                                }
+                            }
+                        }
+                    }
+                }
+                var result = {
+                    "user" : my_user,
+                    "messages" : tabMsg
+                };
+                res.send(result);
+            });
         });
-
     });
 });
-
-router.get('/', function(req, res){
-
-    chatsDB.once('value', function(msgs){
-
-        var messages = {
-            "messages" : msgs.val()
-        };
-        res.send(messages);
-
-    });
-
-});
-
-
 
 module.exports = router;
--- routes/votes.js
+++ routes/votes.js
@@ -5,7 +5,6 @@ var express = require('express');
 var router = express.Router();
 var firebase = require('../firebase/firebase.js');
 var database = firebase.database();
-var eventDB = database.ref().child('events');
 var userDB = database.ref().child('users');
 var groupDB = database.ref().child('groups');
 var voteDB = database.ref().child('votes');
@@ -30,6 +29,22 @@ router.get('/users/:uid/groups/:key', function(req, res){
                      if(my_group.votes){
                          for(var v in my_group.votes){
                              if(all_votes[v]){
+                                 var tabChoix = [];
+                                 if(all_votes[v].choix){
+                                     for(var c in all_votes[v].choix){
+                                         if(all_votes[v].a_vote){
+                                             var pourcent = ( all_votes[v].choix[c] / Object.keys(all_votes[v].a_vote).length ) * 100;
+                                             var choixRes = {
+                                                 "choix" : c,
+                                                 "pourcentage" : pourcent
+                                             }
+                                             tabChoix.push(choixRes);
+                                         }else{
+                                             tabChoix.push(c);
+                                         }
+                                     }
+                                     all_votes[v].choix = tabChoix;
+                                 }
                                  tabVotes.push(all_votes[v]);
                              }
                          }
@@ -53,16 +68,17 @@ router.get('/users/:uid/groups/:key', function(req, res){
 router.post('/', function(req, res) {
 
     var key = req.body.id;
+    var uid = req.body.createur;
     if (key == undefined){
         key = voteDB.push().key;
         voteDB.child(key).child("id").set(key);
-        voteDB.child(key).child("createur").set(req.body.createur);
-
+        voteDB.child(key).child("createur").set(uid);
     }
 
     var groupId = req.body.group;
 
     voteDB.child(key).child("question").set(req.body.question);
+
     var choices = req.body.choix;
     for(var c in choices) {
         voteDB.child(key).child("choix").child(choices[c].choix).set(0);
@@ -70,21 +86,118 @@ router.post('/', function(req, res) {
 
     groupDB.child(groupId).child("votes").child(key).set(true);
 
-    voteDB.once('value', function(snapshot){
-        var votes = snapshot.val();
-        groupDB.once("value", function(snap){
-            var groups = snap.val();
-            var e = {"votes" : votes, "groups" : groups};
-            res.send(e);
+    voteDB.once('value', function(votes){
+        userDB.child(uid).once("value", function(user){
+            groupDB.child(groupid).once("value", function(group){
+                var all_votes = votes.val();
+                var my_user = user.val();
+                var my_group = group.val();
+                var tabVotes = [];
+                if(my_group){
+                    if(all_votes){
+                        if(my_group.votes){
+                            for(var v in my_group.votes){
+                                if(all_votes[v]){
+                                    var tabChoix = [];
+                                    if(all_votes[v].choix){
+                                        for(var c in all_votes[v].choix){
+                                            if(all_votes[v].a_vote){
+                                                var pourcent = (all_votes[v].choix[c] / Object.keys(all_votes[v].a_vote).length) * 100;
+                                                var choixRes = {
+                                                    "choix" : c,
+                                                    "pourcentage" : pourcent
+                                                }
+                                                tabChoix.push(choixRes);
+                                            }else{
+                                                tabChoix.push(c);
+                                            }
+                                        }
+                                        all_votes[v].choix = tabChoix;
+                                    }
+                                    tabVotes.push(all_votes[v]);
+                                }
+                            }
+                        }
+                    }
+                }
+                var result = {
+                    "user": my_user,
+                    "votes": tabVotes
+                };
+                res.send(result);
+
+            });
         });
     });
 });
 
+/**
+ * Supprime un vote
+ */
+router.delete('/:key/groups/:groupid/users/:uid', function(req, res){
+
+    var voteId = req.params.key;
+    var groupId = req.params.groupid;
+    var uid = req.params.uid;
+
+    userDB.child(uid).child("votes").child(voteId).remove();
+    voteDB.child(voteId).remove();
+
+    voteDB.once('value', function(votes){
+        userDB.child(uid).once("value", function(user){
+            groupDB.child(groupid).once("value", function(group){
+                var all_votes = votes.val();
+                var my_user = user.val();
+                var my_group = group.val();
+                var tabVotes = [];
+                if(my_group){
+                    if(all_votes){
+                        if(my_group.votes){
+                            for(var v in my_group.votes){
+                                if(all_votes[v]){
+                                    var tabChoix = [];
+                                    if(all_votes[v].choix){
+                                        for(var c in all_votes[v].choix){
+                                            if(all_votes[v].a_vote){
+                                                var pourcent = (all_votes[v].choix[c] / Object.keys(all_votes[v].a_vote).length) * 100;
+                                                var choixRes = {
+                                                    "choix" : c,
+                                                    "pourcentage" : pourcent
+                                                }
+                                                tabChoix.push(choixRes);
+                                            }else{
+                                                tabChoix.push(c);
+                                            }
+                                        }
+                                        all_votes[v].choix = tabChoix;
+                                    }
+                                    tabVotes.push(all_votes[v]);
+                                }
+                            }
+                        }
+                    }
+                }
+                var result = {
+                    "user": my_user,
+                    "votes": tabVotes
+                };
+                res.send(result);
+
+            });
+        });
+    });
+
+});
+
+/**
+ * Ajoute le vote d'un utilisateur
+ */
 router.post('/users', function(req, res){
 
     var idVotes = req.body.idVote;
     var uid = req.body.uid;
     var reponse = req.body.reponse;
+    var groupid = req.body.group;
 
     var adejavote = false;
 
@@ -108,39 +221,49 @@ router.post('/users', function(req, res){
 
     });
 
-    voteDB.once('value', function(snapshot){
-        var votes = snapshot.val();
-        groupDB.once("value", function(snap){
-            var groups = snap.val();
-            var e = {"votes" : votes, "groups" : groups};
-            res.send(e);
+    voteDB.once('value', function(votes){
+        userDB.child(uid).once("value", function(user){
+            groupDB.child(groupid).once("value", function(group){
+                var all_votes = votes.val();
+                var my_user = user.val();
+                var my_group = group.val();
+                var tabVotes = [];
+                if(my_group){
+                    if(all_votes){
+                        if(my_group.votes){
+                            for(var v in my_group.votes){
+                                if(all_votes[v]){
+                                    var tabChoix = [];
+                                    if(all_votes[v].choix){
+                                        for(var c in all_votes[v].choix){
+                                            if(all_votes[v].a_vote){
+                                                var pourcent = (all_votes[v].choix[c] / Object.keys(all_votes[v].a_vote).length) * 100;
+                                                var choixRes = {
+                                                    "choix" : c,
+                                                    "pourcentage" : pourcent
+                                                }
+                                                tabChoix.push(choixRes);
+                                            }else{
+                                                tabChoix.push(c);
+                                            }
+                                        }
+                                        all_votes[v].choix = tabChoix;
+                                    }
+                                    tabVotes.push(all_votes[v]);
+                                }
+                            }
+                        }
+                    }
+                }
+                var result = {
+                    "user": my_user,
+                    "votes": tabVotes
+                };
+                res.send(result);
+
+            });
         });
     });
-
 });
-
-
-/**
- * Supprime un event
- */
-router.delete('/:id/users/:uid', function(req, res){
-
-    var idVote = req.params.id;
-    var uid = req.params.uid;
-
-    userDB.child(uid).child("votes").child(idVote).remove();
-    voteDB.child(idVote).remove();
-
-    voteDB.once('value', function(snapshot){
-        var votes = snapshot.val();
-        groupDB.once("value", function(snap){
-            var groups = snap.val();
-            var e = {"votes" : votes, "groups" : groups};
-            res.send(e);
-        });
-    });
-
-});
-
 
 module.exports = router;
