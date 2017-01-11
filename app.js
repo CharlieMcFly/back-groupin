74ed7ba58c5fa4de81b1bbccfcf30bc14427bfd7
Charlie Quetstroey <cquetstroey@hotmail.fr>
2017-01-11 14:23:07 +0100

    doc(swagger)  : addding swagger to the project

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+/* SWAGGER*/
+var argv = require('minimist')(process.argv.slice(2));
+var swagger = require("swagger-node-express");
+
+/* ROUTES */
 var index = require('./routes/index');
 var users = require('./routes/users');
 var notifications = require('./routes/notifications');
@@ -13,16 +18,52 @@ var events = require('./routes/events');
 var votes = require('./routes/votes');
 var chats = require('./routes/chats');
 
+/* APP */
 var app = express();
 
-// view engine setup
+/* CORS */
+var cors = require('cors');
+app.use(cors());
+
+
+/* VIEWS */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// CORS
-var cors = require('cors');
+/* SWAGGER DOC */
+var subpath = express();
+app.use(bodyParser());
+app.use("/v1", subpath);
+swagger.setAppHandler(subpath);
+app.use(express.static('dist'));
 
-app.use(cors());
+/* INFO API */
+swagger.setApiInfo({
+    title: "example API",
+    description: "API to do something, manage something...",
+    termsOfServiceUrl: "",
+    contact: "yourname@something.com",
+    license: "",
+    licenseUrl: ""
+});
+
+/* SWAGGER UI*/
+subpath.get('/', function (req, res) {
+    res.sendfile(__dirname + '/dist/index.html');
+});
+
+/* SWAGGER CONFIG */
+swagger.configureSwaggerPaths('', 'api-docs', '');
+
+var domain = 'localhost';
+if(argv.domain !== undefined)
+    domain = argv.domain;
+else
+    console.log('No --domain=xxx specified, taking default hostname "localhost".');
+var applicationUrl = 'http://' + domain;
+swagger.configure(applicationUrl, '1.0.0');
+
+
 
 
 // uncomment after placing your favicon in /public
@@ -31,7 +72,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
@@ -40,6 +81,7 @@ app.use('/groups', groups);
 app.use('/events', events);
 app.use('/votes', votes);
 app.use('/chats', chats);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
